c81b627390a89fa89c53b2b757198c10a1dc3d3a
Syed Ahmed Hussain <ahmedhussaindev@gmail.com>
2017-10-29 15:21:06 -0500

    Initial Commit:
    Added the backend Google Cloud Module.
    Added a Manual  Java library to return jokes called JokeSmithManualProject.
    Added an Android Library called Joke Teller Android Library to tell jokes.

---
+++ FinalProject/JokeSmithsManualProject/src/main/java/com/example/JokeSmith.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+public class JokeSmith {
+
+//    private static MyApi myApiService = null;
+
+    public String getJoke() {
+        return "This is a joke from class \"JokeSmith\" -> method \"getJoke()\"";
+    }
+
+}
--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.JokeSmith;
+import com.joketellerlibrary.JokeTellerActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeTellerActivity.class);
+        intent.putExtra(Intent.EXTRA_REFERRER, new JokeSmith().getJoke());
+        startActivity(intent);
+//        Toast.makeText(this, new JokeSmith().getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
